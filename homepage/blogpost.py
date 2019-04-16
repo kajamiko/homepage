e49b4d7aca2a559e9d3f5c799bfd020d94b3f353
kajamiko <kajaths@gmail.com>
2019-04-16 09:57:06 +0100

    moved class to main file

--- homepage/blogpost.py
+++
@@ -1,9 +0,0 @@
-from extensions import db 
-
-class Blogpost(db.Model):
-
-    __tablename__ = "Blogposts"
-
-    id = db.Column(db.Integer, primary_key=True)
-    
-    content = db.Column(db.String(4096))
--- homepage/run.py
+++ homepage/run.py
@@ -53,6 +53,15 @@ db=SQLAlchemy(app)
 #     return None
 
 
+
+class Blogpost(db.Model):
+
+    __tablename__ = "Blogposts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    
+    content = db.Column(db.String(4096))
+
 @app.route('/')
 def index():
 
