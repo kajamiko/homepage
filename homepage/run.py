5090f510108e9486ab14b97a865fa6b3d63c6cd9
kajamiko <kajaths@gmail.com>
2019-04-16 09:53:20 +0100

    Made temporary changes, removed mail module

--- homepage/run.py
+++ homepage/run.py
@@ -1,72 +1,56 @@
 # -*- coding: utf-8 -*-
 import os
 from socket import gethostname
-from flask_mail import Mail, Message
 from flask import Flask, render_template, url_for, request, redirect, flash, session
 from forms import ContactForm
-from extensions import db
+from flask_sqlalchemy import SQLAlchemy
 from blogpost import Blogpost
 
 
 
-def async_send_mail(app, msg):
-    with app.app_context():
-        mail.send(msg)
+# def async_send_mail(app, msg):
+#     with app.app_context():
+#         mail.send(msg)
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config.update(dict(
+# def create_app():
+app = Flask(__name__)
+app.config.update(dict(
     SQLALCHEMY_DATABASE_URI=str(os.environ.get('SQLALCHEMY_DATABASE_URI')),
     SQLALCHEMY_POOL_RECYCLE=299,
     SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    SECRET_KEY='development key',
-    MAIL_SERVER='smtp.mail.yahoo.com',
-    MAIL_PORT = 465,
-    MAIL_DEFAULT_SENDER = str(os.environ.get('MAIL_USERNAME')),
-    MAIL_USERNAME = str(os.environ.get('MAIL_USERNAME')),
-    MAIL_PASSWORD = str(os.environ.get('MAIL_PASSWORD')),
-    MAIL_USE_TLS = False,
-    MAIL_USE_SSL = True
+    # SECRET_KEY='development key',
+    # MAIL_SERVER='smtp.mail.yahoo.com',
+    # MAIL_PORT = 465,
+    # MAIL_DEFAULT_SENDER = str(os.environ.get('MAIL_USERNAME')),
+    # MAIL_USERNAME = str(os.environ.get('MAIL_USERNAME')),
+    # MAIL_PASSWORD = str(os.environ.get('MAIL_PASSWORD')),
+    # MAIL_USE_TLS = False,
+    # MAIL_USE_SSL = True
     ))
-    register_extensions(app)
-    return app
+    # register_extensions(app)
+    # return app
 
-def register_extensions(app):
-    db.init_app(app)
-# app = Flask(__name__)
-# app.config.update(dict(
-#     SQLALCHEMY_DATABASE_URI=secret.SQLALCHEMY_DATABASE_URI,
-#     SQLALCHEMY_POOL_RECYCLE=299,
-#     SQLALCHEMY_TRACK_MODIFICATIONS=False,
-#     SECRET_KEY='development key',
-#     MAIL_SERVER='smtp.mail.yahoo.com',
-#     MAIL_PORT = 465,
-#     MAIL_DEFAULT_SENDER = str(os.environ.get('MAIL_USERNAME')),
-#     MAIL_USERNAME = str(os.environ.get('MAIL_USERNAME')),
-#     MAIL_PASSWORD = str(os.environ.get('MAIL_PASSWORD')),
-#     MAIL_USE_TLS = False,
-#     MAIL_USE_SSL = True
-# ))
-# db=SQLAlchemy(app)
+# def register_extensions(app):
+#     db.init_app(app)
 
-app = create_app()
-mail = Mail(app)
+# app = create_app()
+# mail = Mail(app)
+db=SQLAlchemy(app)
 
-
-def send_mail(subject, recipient, template, **kwargs):
-    msg = Message(subject, sender=app.config['MAIL_DEFAULT_SENDER'], recipients=[recipient])
-    msg.html = render_template(template, **kwargs)
-    try:
-        mail.send(msg)
-        if session.get('lang') == 'pl':
-            return 'Wiadomość została wysłana.'
-        else:
-            return 'Your message has been sent'
-    except Exception as e:
-        print(e)
-        return "Something went wrong x.x!"
-    return None
+# def send_mail(subject, recipient, template, **kwargs):
+#     msg = Message(subject, sender=app.config['MAIL_DEFAULT_SENDER'], recipients=[recipient])
+#     msg.html = render_template(template, **kwargs)
+#     try:
+#         mail.send(msg)
+#         if session.get('lang') == 'pl':
+#             return 'Wiadomość została wysłana.'
+#         else:
+#             return 'Your message has been sent'
+#     except Exception as e:
+#         print(e)
+#         return "Something went wrong x.x!"
+#     return None
 
 
 @app.route('/')
@@ -82,10 +66,10 @@ def contact():
     form = ContactForm()
     if request.method == 'POST':
         if form.validate_on_submit():
-            send_mail(request.form['subject'], "kajamiko.webdev@gmail.com", 'mail/message.html',
-                    name = request.form['name'],
-                    email = request.form['email'],
-                    message = request.form['message'])
+            # send_mail(request.form['subject'], "kajamiko.webdev@gmail.com", 'mail/message.html',
+            #         name = request.form['name'],
+            #         email = request.form['email'],
+            #         message = request.form['message'])
             return redirect(url_for('success'))
     if session.get('lang') == 'pl':
         return render_template('contact_form_pl.html', form=form)
